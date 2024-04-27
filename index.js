5b4333532957266e703cebff47de5ed9e8a190d2
Sakib3602 <sakibsarker6969@gmail.com>
2024-04-27 12:18:53 +0600

    get data

--- index.js
+++ index.js
@@ -2,6 +2,69 @@ const express = require('express')
 const app = express()
 const port = 9000
 
+const cors = require('cors');
+
+app.use(cors())
+app.use(express.json())
+// A95XIL7VbGicGJjn
+// artandcraft
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://artandcraft:A95XIL7VbGicGJjn@cluster0.b5jufhp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const database = client.db("artandcraft").collection("homecard");
+    
+    // to get home card data from db
+    app.get('/homecard', async(req,res)=>{
+     
+      const cursor = await database.find().toArray();
+      console.log(cursor)
+      res.send(cursor)
+
+    })
+
+    app.get('/homecard/:id', async(req,res)=>{
+      const getId = req.params.id
+      const query = { _id: new ObjectId(getId) };
+      const result = await database.findOne(query);
+      res.send(result)
+    })
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
