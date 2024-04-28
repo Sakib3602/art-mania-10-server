0f8ca467737c3659b185a0572383c6cd66d3a68a
Sakib3602 <sakibsarker6969@gmail.com>
2024-04-28 18:27:52 +0600

    delete

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 9000
+const express = require("express");
+const app = express();
+const port = 9000;
 
-const cors = require('cors');
+const cors = require("cors");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 // A95XIL7VbGicGJjn
 // artandcraft
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://artandcraft:A95XIL7VbGicGJjn@cluster0.b5jufhp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://artandcraft:A95XIL7VbGicGJjn@cluster0.b5jufhp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,103 +28,92 @@ async function run() {
     // await client.connect();
     const database = client.db("artandcraft").collection("homecard");
     const database1 = client.db("artandcraft").collection("additem");
-    
+
     // to get home card data from db
-    app.get('/homecard', async(req,res)=>{
-     
+    app.get("/homecard", async (req, res) => {
       const cursor = await database.find().toArray();
-      
-      res.send(cursor)
 
-    })
+      res.send(cursor);
+    });
 
-    app.get('/homecard/:id', async(req,res)=>{
-      const getId = req.params.id
+    app.get("/homecard/:id", async (req, res) => {
+      const getId = req.params.id;
       const query = { _id: new ObjectId(getId) };
       const result = await database.findOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-
-
-    app.post("/addItemData", async(req,res)=>{
-
+    app.post("/addItemData", async (req, res) => {
       const takeData = req.body;
-      
+
       const result = await database1.insertOne(takeData);
-      res.send(result)
-
-
-    })
-    app.get("/addItemData", async(req,res)=>{
-
+      res.send(result);
+    });
+    app.get("/addItemData", async (req, res) => {
       const cursor = await database1.find().toArray();
-      res.send(cursor)
+      res.send(cursor);
+    });
 
-    })
-
-
-
-
-    app.get("/addItemData/:id", async(req,res)=>{
-      const id = req.params
-      
-      const query = { _id : new ObjectId(id)};
-      const result = await database1.findOne(query)
-      res.send(result)
-
-    })
-
-    app.get('/update/:id', async(req,res)=>{
-      const id = req.params
-      const query = { _id: new ObjectId(id) };
-      const result = await database1.findOne(query)
-      res.send(result)
-
-    })
-
-    app.put("/update/:id", async(req,res)=>{
+    app.get("/addItemData/:id", async (req, res) => {
       const id = req.params;
-      console.log(id)
-      const filter = { _id : new ObjectId(id) };
+
+      const query = { _id: new ObjectId(id) };
+      const result = await database1.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/update/:id", async (req, res) => {
+      const id = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await database1.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const coffee = req.body;
       const updateDoc = {
-      $set: {
-        customization : coffee.customization,
-        email : coffee.email,
-        itemName : coffee.itemName,
-        photo : coffee.photo,
-        price : coffee.price,
-        process : coffee.process,
-        ratings : coffee.ratings,
-        stock : coffee.stock,
-        subcategory : coffee.subcategory,
-        username : coffee.username,
-       
-      },
+        $set: {
+          customization: coffee.customization,
+          email: coffee.email,
+          itemName: coffee.itemName,
+          photo: coffee.photo,
+          price: coffee.price,
+          process: coffee.process,
+          ratings: coffee.ratings,
+          stock: coffee.stock,
+          subcategory: coffee.subcategory,
+          username: coffee.username,
+        },
+      };
 
-    };
+      const result = await database1.updateOne(filter, updateDoc, options);
+      res.send(result);
+      console.log("updated", result);
+    });
 
-    const result = await database1.updateOne(filter, updateDoc, options);
-    res.send(result)
-    console.log("updated",result)
+    app.get("/addItemValue/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const query = { email: email };
+      const result = await database1.find(query).toArray();
+      res.send(result);
+    });
+
+
+
+
+    app.delete('/deletedRoute/:email', async(req,res)=>{
+      const query = { email : req.params.email };
+      console.log(query)
+      const result = await database1.deleteOne(query);
+      res.send(result)
     })
 
 
-
-   
-
-
-    app.get("/addItemValue/:email", async (req, res) => {
-      const email =req.params.email;
-      console.log(email)
-      const query = {email : email}
-      const result = await database1.find(query).toArray()
-      res.send(result)
-    });
-
-   
 
 
 
@@ -135,7 +124,9 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -143,14 +134,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
